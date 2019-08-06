08bf8fe284b64d7ac92f5383720100087ceb07c2
Nikolas Lencioni <nikolaslencioni@MacBook-Air-de-Nikolas.local>
2019-08-06 18:05:39 -0300

    Icones

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Text } from 'react-native'
 import { createAppContainer, createStackNavigator, createDrawerNavigator, createBottomTabNavigator } from 'react-navigation'
 import Icon from 'react-native-vector-icons/Ionicons'
 import './src/config/statusBarConfig'
@@ -20,66 +19,72 @@ const financeiroTabNavigator = createBottomTabNavigator(
 )
 
 const drawerNavigator = createDrawerNavigator(
-{
-   Resumo: Resumo,
-   Configuracoes: Configuracoes,
-   Financeiro: financeiroTabNavigator
-}, {
-   navigationOptions: ({ navigation }) =>  {
-      const { routeName } = navigation.state.routes[navigation.state.index]
-      return {
-         headerTitle: routeName
+   {
+      Resumo: Resumo,
+      Configuracoes: Configuracoes,
+      Financeiro: financeiroTabNavigator
+   }, {
+      navigationOptions: ({ navigation }) => {
+         const { routeName } = navigation.state.routes[navigation.state.index]
+         return {
+            headerTitle: routeName
+         }
       }
-   }
-})
+   })
 
 const stackNavigator = createStackNavigator(
-{
-   Login: {
-      screen: Login,
-      navigationOptions: () => ({
-         title: 'Login',
-         headerBackTitle: null,
-         headerLeft: null,
-         headerStyle: {
-            backgroundColor: '#374a63'
-         },
-         headerTintColor: '#fff'
-      }),
-   },
-   RecoverPassword: {
-      screen: RecoverPassword,
-      navigationOptions: () => ({
-         title: 'Recuperar Senha',
-         headerLeft: null,
-         headerStyle: {
-            backgroundColor: '#374a63'
-         },
-         headerTintColor: '#fff'
-      }),
-   },
-   Drawer: {
-      screen: drawerNavigator,
-      navigationOptions: () => ({
-         title: 'Dashboard',
-         headerStyle: {
-            backgroundColor: '#374a63'
-         },
-         headerTintColor: '#fff'
-      }),
-   }
-}, {
-   defaultNavigationOptions: ({ navigation }) => {
-      return {
-         headerLeft: (
-            <Icon 
-               style={{paddingLeft: 15, color: 'white'}}
-               name="md-menu" 
-               size={30} 
-               onPress={() => navigation.toggleDrawer()}/>
-         )
+   {
+      Login: {
+         screen: Login,
+         navigationOptions: () => ({
+            title: 'Login',
+            headerBackTitle: null,
+            headerLeft: null,
+            headerStyle: {
+               backgroundColor: '#374a63'
+            },
+            headerTintColor: '#fff'
+         }),
+      },
+      RecoverPassword: {
+         screen: RecoverPassword,
+         navigationOptions: ({navigation}) => ({
+            title: 'Recuperar Senha',
+            headerLeft: (
+               <Icon
+                  style={{ paddingLeft: 15, color: 'white' }}
+                  name="md-arrow-back"
+                  size={30}
+                  onPress={() => navigation.goBack(null)} />
+            ),
+            headerStyle: {
+               backgroundColor: '#374a63'
+            },
+            headerTintColor: '#fff',
+         }),
+      },
+      Drawer: {
+         screen: drawerNavigator,
+         navigationOptions: () => ({
+            title: 'Dashboard',
+            headerStyle: {
+               backgroundColor: '#374a63'
+            },
+            headerTintColor: '#fff'
+         })
+      }
+   }, {
+      defaultNavigationOptions: ({ navigation }) => {
+         return {
+            headerLeft: (
+               <Icon
+                  style={{ paddingLeft: 15, color: 'white' }}
+                  name="md-menu"
+                  size={30}
+                  onPress={() => navigation.toggleDrawer()} />
+            )
+         }
       }
    }
-})
-
+)
 export default createAppContainer(stackNavigator)
